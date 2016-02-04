0b530ad5519933ce4dfcbaf733e7759418f68350
Alex Barcelo <alex.barcelo@bsc.es>
2016-02-04 14:58:20 +0100

    adding binaries and structure to everything

--- abdf/__init__.py
+++ abdf/__init__.py
@@ -3,3 +3,17 @@
 All the Logic of Dotfiles management is done throught this library (except the
 special case of bootstrap, which is a standalone script).
 """
+from argparse import ArgumentParser
+
+
+def main():
+    """Execute the main dotfiles
+
+    :return: The exitcode
+    """
+    parser = ArgumentParser(
+        prog="dotfiles",
+        description="Alex Barcelo's Dotfile management library",
+    )
+
+    parser.parse_args()
